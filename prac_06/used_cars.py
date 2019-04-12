a9d03c1a063c280623144cc8ba99672916eae2ad
Jarvis Slockee <jarvis.slockee@my.jcu.edu.au>
2019-04-12 10:33:06 +1000

    - made code appearance better

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -1,11 +1,9 @@
 """CP1404/CP5632 Practical - Client code to use the Car class."""
-# Note that the import has a folder (module) in it.
 
 from prac_06.car import Car
 
 
 def main():
-
     limo = Car("limo", 100)
     limo.add_fuel(20)
     limo.drive(115)
